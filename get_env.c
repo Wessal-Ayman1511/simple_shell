90843c3bbdc57479c9fdb5f6cc5cde044eeee656
Wessal Ayman <wessalayman237@gmail.com>
2023-11-15 12:55:05 -0800

    get environment variables

---
+++ get_env.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * *get_env - function get env variables
+ * @var: variable
+ * Return: enviromental variable
+ */
+
+char *get_env(char *var)
+{
+	int c1 = 0, c2 = 0;
+	int stat;
+
+	while (environ[c1])
+	{
+		stat = 1;
+
+		while (environ[c1][c2] != '=')
+		{
+			if (environ[c1][c2] != var[c2])
+				stat = 0;
+			c2++;
+		}
+		if (stat == 1)
+			break;
+		c1++;
+	}
+	return (&environ[c1][c2 + 1]);
+}
