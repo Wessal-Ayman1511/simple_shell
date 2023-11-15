0fb8dafdf2970aeb6dfb15c6f4fdf0ef881e4c09
Wessal Ayman <wessalayman237@gmail.com>
2023-11-15 12:50:50 -0800

    excution function

---
+++ exe.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _exe - functions that execute
+ * @arguments: arguments
+ * Return:  status
+ */
+
+int _exe(char **arguments)
+{
+	int pid = fork();
+	int stat;
+
+	if (pid == 0)
+	{
+		if (execve(arguments[0], arguments, environ) == -1)
+			perror("Error");
+	}
+	else
+	{
+		wait(&stat);
+		if (WIFEXITED(stat))
+			stat = WEXITSTATUS(stat);
+	}
+
+	return (stat);
+}
