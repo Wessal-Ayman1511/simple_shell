50b7b354eeb8d8076a24d70933d1cc7760039bb5
Wessal Ayman <wessalayman237@gmail.com>
2023-11-15 13:19:18 -0800

    env function

---
+++ print_env.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_env- function print enviroment variables
+*/
+void print_env(void)
+{
+	int c = 0;
+
+	while (environ[c])
+	{
+		write(1, environ[c], str_length(environ[c]));
+		write(1, "\n", 1);
+		c++;
+	}
+}
